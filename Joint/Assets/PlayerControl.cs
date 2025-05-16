7bc4c710185df9cf5c22215cd433c26726a9ed65
Hansen <heshanzhang@outlook.com>
2025-05-16 19:01:00 +0800

    [Hansen] Learn Joint

---
+++ Joint/Assets/PlayerControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        transform.Translate(movement * 2 * Time.deltaTime);
+
+
+    }
+}
